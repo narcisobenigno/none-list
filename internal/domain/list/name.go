355b10a077a02621d74a3b4a07e284b6340b3d1c
Narciso Benigno <narciso.benigno@indebted.co>
2022-12-26 18:25:34 +1100

    feat(internal/domain/list/name): support list name

--- internal/domain/list/name.go
+++ internal/domain/list/name.go
@@ -1,1 +1,24 @@
 package list
+
+import (
+	"errors"
+
+	"github.com/narcisobenigno/none-list/pkg/os/texts"
+)
+
+type Name struct {
+	name texts.Text
+}
+
+func ParseName(name string) (Name, error) {
+	trimmedName := texts.NewSpaceTrimmed(name)
+	if trimmedName.Empty() {
+		return Name{}, errors.New("list name cannot be empty")
+	}
+
+	return Name{name: trimmedName}, nil
+}
+
+func MustParseName(name string) Name {
+	return Name{}
+}
--- internal/domain/list/name_test.go
+++ internal/domain/list/name_test.go
@@ -1,1 +1,47 @@
-package list
+package list_test
+
+import (
+	"testing"
+
+	"github.com/narcisobenigno/none-list/internal/domain/list"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type NameSuite struct {
+	*require.Assertions
+	suite.Suite
+}
+
+func TestNameSuite(t *testing.T) {
+	s := new(NameSuite)
+	s.Assertions = require.New(t)
+	suite.Run(t, s)
+}
+
+func (s *NameSuite) TestParse() {
+	s.Run("different names", func() {
+		name, err := list.ParseName("List Name")
+		s.NoError(err)
+
+		anotherName, err := list.ParseName("Another list Name")
+		s.NoError(err)
+
+		s.NotEqual(name, anotherName)
+	})
+
+	s.Run("equal names", func() {
+		name, err := list.ParseName("List Name")
+		s.NoError(err)
+
+		sameName, err := list.ParseName(" List Name ")
+		s.NoError(err)
+
+		s.Equal(name, sameName)
+	})
+
+	s.Run("returns error when empty name", func() {
+		_, err := list.ParseName("  ")
+		s.EqualError(err, "list name cannot be empty")
+	})
+}
